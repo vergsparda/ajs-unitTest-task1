dc102363073acb0afe8a8ff3ca2792b618fb160d
Alex Smith <alex.vesson.smith@gmail.com>
2020-12-19 17:05:13 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-export default function  setColorLifebar(hero) {
-  if (hero.health < 15){
-   return `critical`;
-  } 
+export default function setColorLifebar(hero) {
+  if (hero.health < 15) {
+    return 'critical';
+  }
   if (hero.health > 50) {
-    return `healthy`;
-  } 
-  return  `wounded`;
+    return 'healthy';
+  }
+  return 'wounded';
 }
